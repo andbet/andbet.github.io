4c3e802c129f4a2487581dd4e75863309efb7a2e
andbet <31863597+andbet@users.noreply.github.com>
2018-01-22 22:47:31 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+// Image switcher code
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+	var mySrc = myImage.getAttribute('src');
+	if(mySrc === 'images/fox.jpg') {
+      myImage.setAttribute ('src','images/dog.jpg');
+	} else {
+	  myImage.setAttribute ('src','images/fox.jpg');
+	}
+}
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
